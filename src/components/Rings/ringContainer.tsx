265b31a0d729ce51661a0de3e80c040ac557e921
Liaqat-dev <dev.liaqat13@gmail.com>
2025-04-01 23:40:36 +0500

    Added Rings

--- src/components/Rings/ringContainer.tsx
+++ src/components/Rings/ringContainer.tsx
@@ -7,24 +7,24 @@ interface Props {
 function RingContainer({children}: Props) {
     return (
         <div className="relative h-[calc(100vh-6rem)] overflow-hidden z-0 ">
-            {/*<Ring size={Math.floor(Math.random() * (10)) + 5} degree={Math.floor(Math.random() * 181)}*/}
-            {/*      position={{top: Math.floor(Math.random() * (50)) + 16, left: Math.floor(Math.random() * (30)) + 10}}*/}
-            {/*      zindex={9}/>*/}
-            {/*{screen.width >= 500 ?*/}
-            {/*    <Ring size={Math.floor(Math.random() * (10)) + 5} degree={Math.floor(Math.random() * 181)}*/}
-            {/*          position={{top: -10, left: (screen.width + Math.floor(Math.random() * (40)) + 100) / 2}}*/}
-            {/*          zindex={9}/> : null}*/}
-            {/*<Ring size={Math.floor(Math.random() * (10)) + 5} degree={Math.floor(Math.random() * 181)} position={{*/}
-            {/*    bottom: Math.floor(Math.random() * (40)) + 10,*/}
-            {/*    right: Math.floor(Math.random() * (40)) + 10*/}
-            {/*}} zindex={9}/>*/}
-            {/*<Ring size={Math.floor(Math.random() * (10)) + 5} degree={Math.floor(Math.random() * 181)}*/}
-            {/*      position={{top: Math.floor(Math.random() * (40)) + 10, right: Math.floor(Math.random() * (40)) + 10}}*/}
-            {/*      zindex={9}/>*/}
-            {/*<Ring size={Math.floor(Math.random() * (10)) + 5} degree={Math.floor(Math.random() * 181)} position={{*/}
-            {/*    bottom: Math.floor(Math.random() * (30)) + 10,*/}
-            {/*    left: Math.floor(Math.random() * (40)) + 10*/}
-            {/*}} zindex={9}/>*/}
+            <Ring size={Math.floor(Math.random() * (10)) + 5} degree={Math.floor(Math.random() * 181)}
+                  position={{top: Math.floor(Math.random() * (50)) + 16, left: Math.floor(Math.random() * (30)) + 10}}
+                  zindex={9}/>
+            {screen.width >= 500 ?
+                <Ring size={Math.floor(Math.random() * (10)) + 5} degree={Math.floor(Math.random() * 181)}
+                      position={{top: -10, left: (screen.width + Math.floor(Math.random() * (40)) + 100) / 2}}
+                      zindex={9}/> : null}
+            <Ring size={Math.floor(Math.random() * (10)) + 5} degree={Math.floor(Math.random() * 181)} position={{
+                bottom: Math.floor(Math.random() * (40)) + 10,
+                right: Math.floor(Math.random() * (40)) + 10
+            }} zindex={9}/>
+            <Ring size={Math.floor(Math.random() * (10)) + 5} degree={Math.floor(Math.random() * 181)}
+                  position={{top: Math.floor(Math.random() * (40)) + 10, right: Math.floor(Math.random() * (40)) + 10}}
+                  zindex={9}/>
+            <Ring size={Math.floor(Math.random() * (10)) + 5} degree={Math.floor(Math.random() * 181)} position={{
+                bottom: Math.floor(Math.random() * (30)) + 10,
+                left: Math.floor(Math.random() * (40)) + 10
+            }} zindex={9}/>
 
 
             {
